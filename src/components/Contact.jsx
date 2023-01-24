866b2f607a96f68f76346c59ee90a2e3e64b3f33
unknown <kasmi_b@etna-alternance.net>
2023-01-24 11:40:27 +0100

    contact me update

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -38,7 +38,7 @@ const Contact = () => {
               className="p-2 bg-transparent border-2 rounded-md text-black focus:outline-none"
             ></textarea>
 
-            <button className="text-white bg-gradient-to-r from-cyan-500 to-violet-700 px-6 py-3 my-8 mx-auto flex items-center rounded-md hover:scale-110 duration-300">
+            <button className="text-white bg-gradient-to-r from-violet-300 to-violet-800 px-6 py-3 my-8 mx-auto flex items-center rounded-md hover:scale-110 duration-300">
               Let's Talk
             </button>
           </form>
