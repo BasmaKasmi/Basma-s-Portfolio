b2b677cb80ce0cb53d282a3b9ed1ef7944bbc37c
unknown <kasmi_b@etna-alternance.net>
2023-03-22 16:14:18 +0100

    update cv

--- src/components/Projets.jsx
+++ src/components/Projets.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import ImgG from '../assets/Gaming.png';
 import webImg from '../assets/website.png';
 import etnaImg from '../assets/etna.png';
-import mImg from '../assets/masterMind.png'
+import em from '../assets/QR.png';
 
 const Projets = () => {
   return (
@@ -15,7 +15,7 @@ const Projets = () => {
           <h2 className='text-3xl uppercase'>Projets</h2>
         </div>
         <div className='grid md:grid-cols-2 gap-4'>
-        <a href="#" className="flex flex-col items-center bg-white border rounded-lg shadow-md md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+        <a href="https://github.com/BasmaKasmi/my-API" className="flex flex-col items-center bg-white border rounded-lg  hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 shadow-md md:flex-row md:max-w-xl">
             <img className="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg" src={webImg} alt=""/>
             <div className="flex flex-col justify-between p-4 leading-normal">
             <ol className='list-disc'>
@@ -25,7 +25,7 @@ const Projets = () => {
               </ol>
             </div>
           </a>
-          <a href="#" className="flex flex-col items-center bg-white border rounded-lg shadow-md md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+          <a href="https://github.com/BasmaKasmi/Code-Camp-etna" className="flex flex-col items-center bg-white border rounded-lg shadow-md md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
             <img className="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg" src={etnaImg} alt=""/>
             <div className="flex flex-col justify-between p-4 leading-normal">
               <ol className='list-disc'>
@@ -36,7 +36,7 @@ const Projets = () => {
               </ol>
             </div>
           </a>
-          <a href="#" className="flex flex-col items-center bg-white border rounded-lg shadow-md md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+          <a href="https://github.com/BasmaKasmi/The-Hyrule-castle" className="flex flex-col items-center bg-white border rounded-lg shadow-md md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
             <img className="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg" src={ImgG} alt=""/>
             <div className="flex flex-col justify-between p-4 leading-normal">
             <ol className='list-disc'>
@@ -46,16 +46,18 @@ const Projets = () => {
               </ol>
             </div>
           </a>
-          <a href="#" className="flex flex-col items-center bg-white border rounded-lg shadow-md md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
-            <img className="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg" src={mImg} alt=""/>
+          <a href="https://github.com/BasmaKasmi/D-fi-ETNA" className="flex flex-col items-center bg-white border rounded-lg shadow-md md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+            <img className="object-cover w-full rounded-t-md h-96 md:h-auto md:w-48 md:rounded-none md:rounded-md" src={em} alt=""/>
             <div className="flex flex-col justify-between p-4 leading-normal">
             <ol className='list-disc'>
-                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Jeu MASTERMIND: programmation d'un jeu en python</h5>
-                <li className="mb-2 font-normal text-gray-700 dark:text-gray-400">Création d'un Jeu Master Mind en Python POO et son interface graphique en Tkinter</li>
-                <li className="mb-2 font-normal text-gray-700 dark:text-gray-400">Technologies utilisées: Python</li>
+                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Défi ETNA: App mobile en React Native, Expo,
+                NodeJs, Typescript</h5>
+                <li className="mb-2 font-normal text-gray-700 dark:text-gray-400">Réalisation d'une application mobile pour rendre le système d'émargement à l'école semi automatique.</li> 
+                <li className="mb-2 font-normal text-gray-700 dark:text-gray-400">Gestion des codes QR présents sur les cartes étudiantes</li>
               </ol>
             </div>
           </a>
+          
 
         </div>
       </div>
