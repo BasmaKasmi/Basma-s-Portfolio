5abc8d0c8ec4a994231c27e6124088a9b3bb8981
Kasmi <kasmi_b@etna-alternance.net>
2023-12-16 23:46:05 +0100

    [edit-code] projects

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -25,7 +25,7 @@ const Hero = () => {
             <p className={`text-xl sm:text-2xl font-bold ${animation} text-gray-700`}>Bonjour, je m'appelle</p>
             <h1 className={`py-3 text-4xl sm:text-6xl md:text-7xl font-bold ${animation} delay-200 text-violet-800`}>Basma KASMI,</h1>
             <h2 className={`text-xl sm:text-2xl font-bold ${animation} delay-400 text-gray-700`}>Développeuse Full Stack,</h2>
-            <a download="" href={Cv} className='btn font-sans inline-block mt-4 py-2 px-8 rounded-full bg-violet-500 hover:bg-violet-600 active:bg-violet-700 focus:outline-none focus:ring focus:ring-violet-300 text-white shadow-lg hover:shadow-xl transition duration-300 ease-in-out transform hover:-translate-y-1'>Télécharger CV</a>
+            <a  target="_blank" rel="noopener noreferrer" download="" href={Cv} className='btn font-sans inline-block mt-4 py-2 px-8 rounded-full bg-violet-500 hover:bg-violet-600 active:bg-violet-700 focus:outline-none focus:ring focus:ring-violet-300 text-white shadow-lg hover:shadow-xl transition duration-300 ease-in-out transform hover:-translate-y-1'>Télécharger CV</a>
         </div>
 
             <div>
@@ -36,8 +36,8 @@ const Hero = () => {
             border border-slate-300 rounded-xl text-center shadow-xl'>
                 <p className='flex justify-center font-semibold text-2xl'>Développement web</p>
                 <div className='flex justify-between flex-wrap px-4'>
-                    <a className='flex px-4 py-2 text-slate-500' href="https://www.wildcodeschool.com/fr-FR/blog/differences-backend-frontend-developpement-web"><DatabaseIcon className='h-6 text-indigo-600' />Back-end</a>
-                    <a className='flex px-4 py-2 text-slate-500' href="https://www.wildcodeschool.com/fr-FR/blog/differences-backend-frontend-developpement-web"><CodeIcon className='h-6 text-indigo-600' />Front-end</a>
+                    <a  target="_blank" rel="noopener noreferrer" className='flex px-4 py-2 text-slate-500' href="https://www.wildcodeschool.com/fr-FR/blog/differences-backend-frontend-developpement-web"><DatabaseIcon className='h-6 text-indigo-600' />Back-end</a>
+                    <a  target="_blank" rel="noopener noreferrer" className='flex px-4 py-2 text-slate-500' href="https://www.wildcodeschool.com/fr-FR/blog/differences-backend-frontend-developpement-web"><CodeIcon className='h-6 text-indigo-600' />Front-end</a>
                 </div>
             </div>
         </div>
--- src/components/Projets.jsx
+++ src/components/Projets.jsx
@@ -6,59 +6,84 @@ import etnaImg from '../assets/etna.png';
 import em from '../assets/QR.png';
 
 const Projets = () => {
+
+  const projects = [
+    {
+      id: 1,
+      title: "API : Développement d'un site E-commerce",
+      description: [
+        "Séquelisation Création de schémas de base de données avec MySQL.",
+
+        "Pour le backend : NodeJs, MySql, Sequelize, ExpressJs",
+        "Pour le frontend: Typescipt, NextJs"
+        
+      ],
+      image: webImg,
+      link: "https://github.com/BasmaKasmi/my-API", 
+    },
+    {
+      id: 2,
+      title: "Pôle Suivi : Effectuer des mêlées de suivi pédagogique avec les étudiants.",
+      description: [
+        "Pour le backend : MySQL, NodeJS, TypeScript et ExpressJS.",
+        "Pour le frontend : JavaScript, ReactJs, CSS"
+      ],
+      image: etnaImg,
+      link: " https://github.com/BasmaKasmi/Code-Camp-etna",
+    },
+    {
+      id: 3,
+      title: "The Hyrule Castle: Développement de jeux en TypeScript",
+      description: [
+        "Gérer les entrées utilisateur et leur gestion des erreurs. Créer un gameplay autour de fonctions algorithmiques et mathématiques, en utilisant le langage de programmation Typescript",
+      ],
+      image: ImgG,
+      link: "https://github.com/BasmaKasmi/The-Hyrule-castle",
+    },
+    {
+      id: 4,
+      title: "Défi ETNA code camp: App mobile en React Native, Expo, NodeJs, Typescript",
+      description: [
+        "Réalisation d'une application mobile pour rendre le système d'émargement à l'école semi automatique. Gestion des codes QR présents sur les cartes étudiantes",
+      ],
+      image: em,
+      link: "https://github.com/BasmaKasmi/D-fi-ETNA",
+    }
+  ];
+
+  
   return (
     <div name='Projets' className='w-full text-white my-24'>
-      <div className='w-full h-[800px] bg-slate-900 absolute mix-blend-overlay'></div>
+      <div className='w-full h-[100%] absolute mix-blend-overlay'></div>
 
       <div className='max-w-[1240px] mx-auto py-2 '>
         <div className='text-center py-4 text-slate-300'>
-          <h2 className='text-3xl uppercase'>Projets</h2>
+          <h2 className='text-3xl  text-black uppercase'>Projets</h2>
         </div>
-        <div className='grid md:grid-cols-2 gap-4'>
-        <a href="#" className="flex flex-col items-center bg-white border rounded-lg md:flex-row md:max-w-xl">
-            <img className="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg" src={webImg} alt=""/>
-            <div className="flex flex-col justify-between p-4 leading-normal ">
-            <ol className='list-disc'>
-                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">API : Développement d'un site E-commerce</h5>
-                <li className="mb-2 font-normal text-gray-700 dark:text-gray-400">Apprenez à développer une API avec NodeJS, TypeScript et ExpressJS.</li>
-                <li className="mb-2 font-normal text-gray-700 dark:text-gray-400">Séquelisation Création de schémas de base de données avec MySQL</li>
-              </ol>
-            </div>
-          </a>
-          <a href="#" className="flex flex-col items-center bg-white border rounded-lg md:flex-row md:max-w-xl">
-            <img className="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg" src={etnaImg} alt=""/>
-            <div className="flex flex-col justify-between p-4 leading-normal">
-              <ol className='list-disc'>
-                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"> Pôle Suivi : Effectuer des mêlées de suivi pédagogique avec les étudiants.</h5>
-                <li className="mb-2 font-normal text-gray-700 dark:text-gray-400">Création d'un backend (API) et d'un frontend (web) en utilisant les technologies suivantes:</li>
-                <li className="mb-2 font-normal text-gray-700 dark:text-gray-400">Pour le backend : MySQL, NodeJS, TypeScript et ExpressJS.<br /> Pour le frontend : JavaScript, ReactJs, CSS</li>
-
-              </ol>
-            </div>
-          </a>
-          <a href="#" className="flex flex-col items-center bg-white border rounded-lg md:flex-row md:max-w-xl">
-            <img className="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg" src={ImgG} alt=""/>
-            <div className="flex flex-col justify-between p-4 leading-normal">
-            <ol className='list-disc'>
-                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">The Hyrule Castle: Développement de jeux en TypeScript</h5>
-                <li className="mb-2 font-normal text-gray-700 dark:text-gray-400">Gérer les entrées utilisateur et leur gestion des erreurs</li>
-                <li className="mb-2 font-normal text-gray-700 dark:text-gray-400">Créer un gameplay autour de fonctions algorithmiques et mathématiques</li>
-              </ol>
-            </div>
-          </a>
-          <a href="#" className="flex flex-col items-center bg-white border rounded-lg md:flex-row md:max-w-xl">
-            <img className="object-cover w-full rounded-t-md h-96 md:h-auto md:w-48 md:rounded-none md:rounded-md" src={em} alt=""/>
-            <div className="flex flex-col justify-between p-4 leading-normal">
-            <ol className='list-disc'>
-                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Défi ETNA code camp: App mobile en React Native, Expo,
-                NodeJs, Typescript</h5>
-                <li className="mb-2 font-normal text-gray-700 dark:text-gray-400">Réalisation d'une application mobile pour rendre le système d'émargement à l'école semi automatique.</li> 
-                <li className="mb-2 font-normal text-gray-700 dark:text-gray-400">Gestion des codes QR présents sur les cartes étudiantes</li>
-              </ol>
-            </div>
-          </a>
-          
-
+        <div className='grid md:grid-cols-2 gap-4 px-4 py-12'>
+        {projects.map((project) => (
+  <div key={project.id} className="group bg-white border rounded-lg shadow-md overflow-hidden transform transition duration-500 hover:scale-105">
+    <img className="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg" src={project.image} alt={project.title}/>
+    <div className="flex flex-col justify-between p-4 leading-normal ">
+      <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 group-hover:text-indigo-600 transition-colors duration-300">{project.title}</h5>
+      <div className="text-gray-700">
+        {project.description.map((line, index) => (
+          <p key={index} className="mb-2 font-normal">{line}</p>
+        ))}
+      </div>
+      <div className="mt-4 self-end">
+      <a
+      href={project.link}
+      target="_blank"
+      rel="noopener noreferrer"
+      className="flex flex-col justify-center items-center btn font-sans py-2 px-8 rounded-full bg-violet-500 hover:bg-violet-600 active:bg-violet-700 focus:outline-none focus:ring focus:ring-violet-300 text-white shadow-lg hover:shadow-xl transition duration-300 ease-in-out transform hover:-translate-y-1"
+      >
+        Voir le projet
+      </a>
+      </div>
+    </div>
+  </div>
+))}
         </div>
       </div>
     </div>
