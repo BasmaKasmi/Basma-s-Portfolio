b33e74b661cbdf8e18fdb21906c7216422330fe4
Kasmi <kasmi_b@etna-alternance.net>
2023-12-21 22:52:35 +0100

    fix footer links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,75 +1,74 @@
-import React from 'react';
-import logo from '../assets/logo.png';
-
-const contactInfos = [
-  { id: 1, href: "#", text: "0634267644" },
-  { id: 2, href: "mailto:kasmi_b@etna-alternance.net", text: "kasmi_b@etna-alternance.net" }
-];
-
-const resources = [
-  { id: 1, href: "https://tailwindcss.com/", text: "Tailwind CSS" },
-  { id: 2, href: "https://www.manypixels.co/gallery", text: "Many pixels" }
-];
-
-const socialLinks = [
-  { id: 1, href: "https://github.com/BasmaKasmi", text: "GitHub account", svgPath: "M12 2C6.477 2 2 6.484 2 12.017c0 4.425 ...", svgViewBox: "0 0 24 24" },
-  { id: 2, href: "https://www.linkedin.com/in/basma-kasmi-38aba923b/", text: "LinkedIn page", svgPath: "M19 0h-14c-1.1 0-2 .9-2 2v20c1.1 0 2-.9 2-2h14 ...", svgViewBox: "0 0 24 24" },
-  { id: 3, href: "https://www.instagram.com/itsbasmawhat/", text: "Instagram page", svgPath: "M12.315 2c2.43 0 2.784.013 3.808.06 ...", svgViewBox: "0 0 24 24" },
-];
+import React from 'react'
+import logo from '../assets/logo.png'
 
 const Footer = () => {
     return (
-        <footer className="p-4 sm:p-6 bg-zinc-200">
-            <div className="md:flex md:justify-between">
-                <div className="mb-6 md:mb-0">
-                    <a href="/" className="flex items-center">
-                        <img src={logo} className="h-9 mr-3 hover:scale-110 transition-transform duration-150" alt="Logo" />
-                        <span className="self-center text-2xl font-semibold whitespace-nowrap">Basma KASMI</span>
-                    </a>
-                </div>
-                <div className="grid grid-cols-3 gap-8 sm:gap-6 sm:grid-cols-3">
-                    <div>
-                        <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase">Contact me</h2>
-                        <ul className="text-gray-600">
-                            {contactInfos.map(info => (
-                                <li key={info.id}>
-                                    <a href={info.href} className="hover:underline">{info.text}</a>
-                                </li>
-                            ))}
-                        </ul>
-                    </div>
-                    <div>
-                        <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase">Resources</h2>
-                        <ul className="text-gray-600">
-                            {resources.map(resource => (
-                                <li key={resource.id}>
-                                    <a target="_blank" rel="noopener noreferrer" href={resource.href} className="hover:underline">{resource.text}</a>
-                                </li>
-                            ))}
-                        </ul>
-                    </div>
-                    <div>
-                        <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase">Follow me</h2>
-                        <ul className="text-gray-600">
-                            {socialLinks.map(link => (
-                                <li key={link.id}>
-                                    <a target="_blank" rel="noopener noreferrer" href={link.href} className="hover:text-gray-900 dark:hover:text-white">
-                                        <svg className="w-5 h-5" fill="currentColor" viewBox={link.svgViewBox} aria-hidden="true">
-                                            <path d={link.svgPath}></path>
-                                        </svg>
-                                        <span className="sr-only">{link.text}</span>
-                                    </a>
-                                </li>
-                            ))}
-                        </ul>
-                    </div>
-                </div>
+    <footer className="p-4 bg-white sm:p-6 bg-zinc-200 ">
+    <div className="md:flex md:justify-between">
+        <div className="mb-6 md:mb-0">
+            <a href="" className="flex items-center">
+                <img src={logo} className="h-9 mr-3" alt="Logo" />
+                <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Basma KASMI</span>
+            </a>
+        </div>
+        <div className="grid grid-cols-3 gap-8 sm:gap-6 sm:grid-cols-3">
+            <div>
+                <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Contact me</h2>
+                <ul className="text-gray-600 dark:text-gray-400">
+                    <li>
+                        <a href="#" className="hover:underline">0634267644</a>
+                    </li>
+                    <li>
+                        <a href="#" className="hover:underline">kasmi_b@etna-alternance.net</a>
+                    </li>
+                </ul>
             </div>
-            <hr className="my-6 border-gray-200 sm:mx-auto lg:my-8" />
-            <div className="sm:flex sm:items-center sm:justify-between">
-                <span className="text-sm text-gray-500 sm:text-center">© 2023 BK. All Rights Reserved.</span>
+            <div>
+                <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Resources</h2>
+                <ul className="text-gray-600 dark:text-gray-400">
+                    <li>
+                        <a target="_blank" rel="noopener noreferrer" href="https://tailwindcss.com/" className="hover:underline">Tailwind CSS</a>
+                    </li>
+                    <li>
+                        <a target="_blank" rel="noopener noreferrer" href="https://www.manypixels.co/gallery" className="hover:underline">Many pixels</a>
+                    </li>
+                </ul>
             </div>
-        </footer>
+            <div>
+                <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Follow me</h2>
+                <ul className="text-gray-600 dark:text-gray-400">
+                    <li>
+                        <a target="_blank" rel="noopener noreferrer" href="https://github.com/BasmaKasmi" className="hover:underline ">Github</a>
+                    </li>
+                    <li>
+                        <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/basma-kasmi-38aba923b/" className="hover:underline">LinkedIn</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+    <div className="sm:flex sm:items-center sm:justify-between">
+        <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="http://localhost:3000/" className="hover:underline">BK</a>. All Rights Reserved.
+        </span>
+        <div className="flex mt-4 space-x-6 sm:justify-center sm:mt-0">
+            <a target="_blank" rel="noopener noreferrer" href="https://www.instagram.com/itsbasmawhat/" class="text-gray-500 hover:text-gray-900 dark:hover:text-white">
+                <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true"><path fill-rule="evenodd" d="M12.315 2c2.43 0 2.784.013 3.808.06 1.064.049 1.791.218 2.427.465a4.902 4.902 0 011.772 1.153 4.902 4.902 0 011.153 1.772c.247.636.416 1.363.465 2.427.048 1.067.06 1.407.06 4.123v.08c0 2.643-.012 2.987-.06 4.043-.049 1.064-.218 1.791-.465 2.427a4.902 4.902 0 01-1.153 1.772 4.902 4.902 0 01-1.772 1.153c-.636.247-1.363.416-2.427.465-1.067.048-1.407.06-4.123.06h-.08c-2.643 0-2.987-.012-4.043-.06-1.064-.049-1.791-.218-2.427-.465a4.902 4.902 0 01-1.772-1.153 4.902 4.902 0 01-1.153-1.772c-.247-.636-.416-1.363-.465-2.427-.047-1.024-.06-1.379-.06-3.808v-.63c0-2.43.013-2.784.06-3.808.049-1.064.218-1.791.465-2.427a4.902 4.902 0 011.153-1.772A4.902 4.902 0 015.45 2.525c.636-.247 1.363-.416 2.427-.465C8.901 2.013 9.256 2 11.685 2h.63zm-.081 1.802h-.468c-2.456 0-2.784.011-3.807.058-.975.045-1.504.207-1.857.344-.467.182-.8.398-1.15.748-.35.35-.566.683-.748 1.15-.137.353-.3.882-.344 1.857-.047 1.023-.058 1.351-.058 3.807v.468c0 2.456.011 2.784.058 3.807.045.975.207 1.504.344 1.857.182.466.399.8.748 1.15.35.35.683.566 1.15.748.353.137.882.3 1.857.344 1.054.048 1.37.058 4.041.058h.08c2.597 0 2.917-.01 3.96-.058.976-.045 1.505-.207 1.858-.344.466-.182.8-.398 1.15-.748.35-.35.566-.683.748-1.15.137-.353.3-.882.344-1.857.048-1.055.058-1.37.058-4.041v-.08c0-2.597-.01-2.917-.058-3.96-.045-.976-.207-1.505-.344-1.858a3.097 3.097 0 00-.748-1.15 3.098 3.098 0 00-1.15-.748c-.353-.137-.882-.3-1.857-.344-1.023-.047-1.351-.058-3.807-.058zM12 6.865a5.135 5.135 0 110 10.27 5.135 5.135 0 010-10.27zm0 1.802a3.333 3.333 0 100 6.666 3.333 3.333 0 000-6.666zm5.338-3.205a1.2 1.2 0 110 2.4 1.2 1.2 0 010-2.4z" clip-rule="evenodd" /></svg>
+                <span class="sr-only">Instagram page</span>
+            </a>
+            <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/basma-kasmi-38aba923b/" className="text-gray-500 hover:text-gray-900 dark:hover:text-white">
+            <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
+                <path d="M19 0h-14c-1.1 0-2 .9-2 2v20c1.1 0 2-.9 2-2h14c1.1 0 2-.9 2-2v-20c0-1.1-.9-2-2-2zm-13 19h-3v-10h3v10zm-1.5-11.5c-1.1 0-1.8-.9-1.8-1.8s.9-1.8 1.8-1.8 1.8.9 1.8 1.8-.9 1.8-1.8 1.8zm13.5 11.5h-3v-5.5c0-1.4-.5-2.5-1.8-2.5-1 0-1.5.7-1.8 1.3-.1.2-.1.5-.1.7v6h-3s.1-10 0-11h3v1.6c.4-.7 1.2-1.6 2.9-1.6 2.1 0 3.7 1.4 3.7 4.3v6.7z"/>
+            </svg>
+                <span className="sr-only">Linkedin page</span>
+            </a>
+            <a target="_blank" rel="noopener noreferrer" href="https://github.com/BasmaKasmi" className="text-gray-500 hover:text-gray-900 dark:hover:text-white">
+                <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true"><path fill-rule="evenodd" d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z" clip-rule="evenodd" /></svg>
+                <span className="sr-only">GitHub account</span>
+            </a>
+        </div>
+    </div>
+</footer>
     )
 }
 
