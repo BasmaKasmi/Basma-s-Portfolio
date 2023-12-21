b6c4350626dd44607eee3dee879e05f0ee90e754
Kasmi <kasmi_b@etna-alternance.net>
2023-12-22 00:02:26 +0100

    fix footer competences

--- src/components/Comp.jsx
+++ src/components/Comp.jsx
@@ -40,7 +40,6 @@ const Comp = () => {
       <div className='max-w-[1240px] mx-auto text-white relative'>
           <div className='px-4 py-12'>
               <h2 className='text-3xl pt-8 text-slate-300 uppercase text-center'>Compétences</h2>
-              <h3 className='text-5xl font-bold py-6 text-left'>Every skill you acquire doubles your odds of success. <br />-Scott Adams</h3>
           </div>
 
           <div className='grid grid-cols-1 lg:grid-cols-3 relative gap-x-8 gap-y-16 px-4 pt-12 sm:pt-20 text-black'>
